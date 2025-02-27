3abc6027f27cf9813bc1f4e9d8fbbf650a301508
Karthik <karthik.veeranki.2003@gmail.com>
2025-02-27 21:40:49 +0530

    Added algorithm steps in app.py

--- app.py
+++ app.py
@@ -13,25 +13,27 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def upload_files():
+    # Step 1: take the files input
     file1 = request.files['file1']
     file2 = request.files['file2']
 
     content1 = file1.read().decode('utf-8')
     content2 = file2.read().decode('utf-8')
 
+    # Step 2: Clean and fetch the data for commit_id
     commits1 = logic.compute(content1, True)
     commits2 = logic.compute(content2, False)
 
-    # Now we need to feed the commits of both files as input to find the commits present only in file 1 and not in file 2
+    # Step 3: feed the commits of both files as input 
+    # to find the commits present only in file 1 and not in file 2
     sorted_result = logic.findDifference(commits1, commits2)
 
-    # convert this result into a string and pass to html content
+    # Step 4: Save the result in a file "output.txt"
     logic.saveContents(sorted_result, file1, file2)
 
-    # calling the html table function
+    # Step 5: Create the result html page dynamically
     html_table.html_call(sorted_result)
     
-    # return render_template('result.html', content=final_result)
     return render_template('result.html')
 
 
